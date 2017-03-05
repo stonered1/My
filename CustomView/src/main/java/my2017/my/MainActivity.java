82d7a52b61c54c4423175b2e4c6de203652903a6
Stone <reddstones@163.com>
2017-03-05 16:54:18 +0800

    ---

--- CustomView/src/main/java/my2017/my/MainActivity.java
+++ CustomView/src/main/java/my2017/my/MainActivity.java
@@ -1,20 +1,24 @@
 package my2017.my;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import my2017.my.uitl.CustomTitleBar;
+import my2017.my.uitl.TagsLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_silder_bar);
 
-        CustomTitleBar customTitleBar = (CustomTitleBar) findViewById(R.id.my_titlebar);
+        /*CustomTitleBar customTitleBar = (CustomTitleBar) findViewById(R.id.my_titlebar);
         customTitleBar.setTitleClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,6 +30,18 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
                 }
             }
-        });
+        });*/
+
+
+        /*TagsLayout imageViewGroup = (TagsLayout) findViewById(R.id.image_layout);
+        ViewGroup.MarginLayoutParams lp = new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        String[] string={"从我写代码那天起，我就没有打算写代码","从我写代码那天起","我就没有打算写代码","没打算","写代码"};
+        for (int i = 0; i < 5; i++) {
+            TextView textView = new TextView(this);
+            textView.setText(string[i]);
+            textView.setTextColor(Color.WHITE);
+            textView.setBackgroundColor(Color.BLUE);
+            imageViewGroup.addView(textView, lp);
+        }*/
     }
 }
---
+++ CustomView/src/main/java/my2017/my/uitl/SilderBar.java
@@ -0,0 +1,59 @@
+package my2017.my.uitl;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Stone on 17/3/5.
+ */
+
+public class SilderBar extends View {
+
+    //设置显示的字母
+    private String[] strs = {"#", "A", "B", "C", "D", "E",
+            "F", "G", "H", "I", "G",
+            "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"
+    };
+
+    private Paint mPaint;
+
+    public SilderBar(Context context) {
+        super(context, null);
+    }
+
+    public SilderBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init(){
+        mPaint = new Paint();
+        mPaint.setColor(Color.GRAY);
+        mPaint.setAntiAlias(true);
+        mPaint.setTextAlign(Paint.Align.CENTER);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+
+        if ()
+    }
+
+    /*@Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }*/
+}
